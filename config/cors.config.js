9866487fd2c3d3f0b898adc8d73751b40a5a6744
thomas <thomasbastien17@gmail.com>
2025-01-05 15:01:17 +0100

    add domain front in cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://roje6147.odns.fr'], // adresse du serveur front
+  origin: ['dice-forge-front.vercel.app', 'http://roje6147.odns.fr'], // adresse du serveur front
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
   credentials: true, 
